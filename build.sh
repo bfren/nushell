ccc0da5133f68845f22d320fe4d12180872c563e
bfren <bf@bfren.dev>
2024-02-07 08:01:58 +0000

    Aligning comments

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 N=${1}                                  # Nushell version
 P=linux/amd64,linux/arm/v7,linux/arm64  # Platforms to build
-T=ghcr.io/bfren/nushell:${N}     # Tag to use
+T=ghcr.io/bfren/nushell:${N}            # Tag to use
 
 docker buildx build --build-arg NUSHELL=${N} --platform ${P} --tag ${T} --push .
